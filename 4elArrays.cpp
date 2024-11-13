9dfc5b3f9ac7751286edcc6f499e3bc0a3d1475a
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-13 13:37:00 -0500

    Create 4elArrays.cpp

---
+++ 4elArrays.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[4] = {1, 2, 3, 4}; // Declaration and initialization
+    for(int i = 0; i < 4; i++) {
+        cout << numbers[i] << " ";
+    }
+    return 0;
+}
