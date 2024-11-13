99adecaf254ff24ddb165c33b55892046a736cd8
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-13 15:09:38 -0500

    Create funcOveride.cpp
    Function overriding allows a derived class to provide a specific implementation of a method that is already defined in its base class. This is achieved using the virtual `keyword` in the base class.
    
    Here, the `show` function in the `Derived` class overrides the `show` function in the Base class.

---
+++ Inheritance/funcOveride.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void show() {
+        cout << "Base class show function" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() override {
+        cout << "Derived class show function" << endl;
+    }
+};
+
+int main() {
+    Base* basePtr;
+    Derived derivedObj;
+
+    basePtr = &derivedObj;
+    basePtr->show(); // Calls Derived's show function
+
+    return 0;
+}
